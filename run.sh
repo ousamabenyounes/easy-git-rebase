f99c165c5d96cf3abfc9ff30f3ba722601d4e700
Ben Younes Ousama <benyounes.ousama@gmail.com>
2019-02-10 22:07:00 +0100

    feature: checking current branch if allready on destination branch

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+###########################################################################################
+# Date         Version    Author             Description
+#------------------------------------------------------------------------------------------
+# 2019/02/10   1.0        Ousama BEN YOUNES  Git Rebase simple script 
+###########################################################################################
 
-
-DESTINATION="develop"
+DESTINATION_BRANCH="develop"
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
+if [ "$DESTINATION_BRANCH" == "$CURRENT_BRANCH" ]; then
+    echo "Current branch & destination branch are the same..."
+    exit 0
+fi
 
 ##################################################################
 # Purpose: Launch given command, print it or exit if error occurs
@@ -24,7 +32,7 @@ function launch_cmd()
     fi
 }
 
-launch_cmd "git checkout $DESTINATION"
-launch_cmd "git pull origin $DESTINATION"
+launch_cmd "git checkout $DESTINATION_BRANCH"
+launch_cmd "git pull origin $DESTINATION_BRANCH"
 launch_cmd "git checkout $CURRENT_BRANCH"
-launch_cmd "git rebase $DESTINATION"
+launch_cmd "git rebase $DESTINATION_BRANCH"
